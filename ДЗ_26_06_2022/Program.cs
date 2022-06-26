e5c589cec241d0db1131ffa4b9a5f0a26d3659bd
StraNn1k7643 <107756482+StraNn1k7643@users.noreply.github.com>
2022-06-26 13:27:47 +0300

    Добавил дз 26.06.2022

---
+++ ДЗ_26_06_2022/Program.cs
@@ -0,0 +1,74 @@
+﻿// //Задача №15
+// Console.Write("Введите день недели ") ;
+// int dayNumber = Convert.ToInt32(Console.ReadLine()) ;
+
+// if (dayNumber >= 1 && dayNumber <= 7) 
+// {
+//     if (dayNumber == 1 )
+//     {
+//         Console.WriteLine("Понедельник") ;
+//     }
+//     if (dayNumber == 2 )
+//     {
+//         Console.WriteLine("Вторник") ;
+//     }
+//     if (dayNumber == 3 )
+//     {
+//         Console.WriteLine("Среда") ;
+//     }
+//     if (dayNumber == 4 )
+//     {
+//         Console.WriteLine("Четверг") ;
+//     }
+//     if (dayNumber == 5 )
+//     {
+//         Console.WriteLine("Пятница") ;
+//     }
+//     if (dayNumber == 6 )
+//     {
+//         Console.WriteLine("Суббота,выходной день") ;
+//     }
+//     if (dayNumber == 7 )
+//     {
+//         Console.WriteLine("Воскресенье,выходной день") ;
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Error 404, day not found") ;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// //Задача №10
+// Console.Write("Введи трехзначное число: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// string arraynumber = Convert.ToString(Number);
+// Console.WriteLine("вторая цифра числа " +arraynumber[1]);
+
+
+
+
+
+
+
+// //Задача №13
+// Console.Write("Введи число ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// string NumberText = Convert.ToString(Number);
+// if (NumberText.Length > 2)
+// {
+//   Console.WriteLine("третья цифра " + NumberText[2] );
+// }
+// else 
+// {
+//   Console.WriteLine("третья цифра отсутствует");
+// }
